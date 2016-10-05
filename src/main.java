713894781335647ade5fe07411a783a8b14a92c4
alvaro.montes <alvaro.montes@WKSALMPBB915.u-tad.es>
2016-10-05 14:20:18 +0200

    primer commit. clases creadas y main empezado

---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+	public static void main(String[] args) {
+		boton btn = new boton();
+		clasificador clasif = new clasificador();
+		deposito dep = new deposito();
+		dispensador disp = new dispensador();
+		retorno ret = new retorno();
+		seleccion sel = new seleccion();
+		visor vis = new visor();
+		
+		// setters
+	}
+}
